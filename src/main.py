8bfcfd8b72624af8cb532234fb3e77d5fd52d17f
Micha <177941509+2mjlux@users.noreply.github.com>
2026-02-09 10:52:31 +0100

    Add textnode.py and main.py, delete text content of public folder

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from textnode.py import TextNode, TextType
+
+
+def main():
+    test_node = TextNode(
+        "This is some anchor text",
+        "TextType.LINK",
+        "https://www.boot.dev"
+    )
+
+
+main()
