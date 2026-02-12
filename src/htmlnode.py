a7f09cec1555cb561a6e69368868b23857858e1e
Micha <177941509+2mjlux@users.noreply.github.com>
2026-02-12 18:58:01 +0100

    Add textnode test files and htmlnode.py

---
+++ src/htmlnode.py
@@ -0,0 +1,24 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+
+    def props_to_html(self):
+        if self.props == None or self.props == {}:
+            return ""
+        result = ""
+        for key, value in self.props.items():
+            result = result + " " + key + '"' + value + '"'
+        return result
+
+    def __repr__(self):
+        print("This HTMLNode object has the following data members:")
+        print(f"tag: {self.tag}")
+        print(f"value: {self.value}")
+        print(f"children: {self.children}")
+        print(f"props: {self.props}")
