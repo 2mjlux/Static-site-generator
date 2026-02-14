d7151763a33cc8baa3f02a3dbbcf0909cca50a44
Micha <177941509+2mjlux@users.noreply.github.com>
2026-02-14 19:34:40 +0100

    Further work on test_htmlnode.py, in progress

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -4,27 +4,22 @@ from htmlnode import HTMLNode
 
 
 class TestHTMLNode(unittest.TestCase):
-    def test_props(self):
-        #node = TextNode("This is a text node", TextType.BOLD)
-        #node2 = TextNode("This is a text node", TextType.BOLD)
-        #self.assertEqual(node, node2)
+    def test_props_normal(self):
+        test_props = {"href": "https://www.google.com", "target": "_blank", }
+        node = HTMLNode(None, None, None, test_props)
+        self.assertEqual(
+            node.props_to_html(node),
+            'href="https://www.google.com" target="_blank"'
+        )
 
-    def test_not_props(self):
-        #node = TextNode("This is a text node", TextType.BOLD)
-        #node2 = TextNode("This is a text node", TextType.ITALIC)
-        #self.assertNotEqual(node, node2)
+    def test_props_none(self):
+        node = HTMLNode(None, None, None, None)
+        self.assertEqual(node.props_to_html(node), '')
 
-    def test_not_props2(self):
-        #node = TextNode("This is a text node", TextType.BOLD)
-        #node2 = TextNode("This is a not text node", TextType.BOLD)
-        #self.assertNotEqual(node, node2)
-
-    def test_props2(self):
-        #node = TextNode("This is a text node", TextType.BOLD, None)
-        #node2 = TextNode("This is a text node", TextType.BOLD)
-        #self.assertEqual(node, node2)
+    def test_props_empty(self):
+        node = HTMLNode(None, None, None)
+        self.assertEqual(node.props_to_html(node), '')
 
 
 if __name__ == "__main__":
     unittest.main()
-
